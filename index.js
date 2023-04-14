cbc559509ceeff833ae88da2f7c6c8fac840d09e
Aditya <adityavishal903@gmail.com>
2023-04-14 23:16:16 +0530

    .

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-//javascript console API
-console.log("Hello world")
+//way to printing
 //alert("Me")
 // document.write("This is write");
+
+//javascript console API
+console.log("Hello world")
 console.warn("This is Warning!");
 console.error("This is error!");
