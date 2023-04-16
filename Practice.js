4c59b2121d1dcc7ecb8067f9b4d256d22d9f747d
Aditya <adityavishal903@gmail.com>
2023-04-16 18:31:38 +0530

    .

--- Practice.js
+++ Practice.js
@@ -4,6 +4,11 @@
 // document.write("This is write");
 
 //javascript console API
-console.log("Hello world")
-console.warn("This is Warning!");
-console.error("This is error!");
+// console.log("Hello world")
+// console.warn("This is Warning!");
+// console.error("This is error!");
+
+// variable
+var a1=20;
+var a2=30;
+console.log(a1+a2);
